51d888212ed0e4ac40193e322cb4b6681e4a1881
winkxx <68945988+winkxx@users.noreply.github.com>
2022-01-25 16:04:48 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+# UUID="00277430-85b5-46e2-a6c9-4fe3da538187"
+# APP_NAME="lyz7805-v2ray"
+REGION="hkg"
+
+if ! command -v flyctl >/dev/null 2>&1; then
+    printf '\e[33mCould not resolve command - flyctl. So, install flyctl first.\n\e[0m'
+    curl -L https://fly.io/install.sh | FLYCTL_INSTALL=/usr/local sh
+fi
+
+if [ -z "${APP_NAME}" ]; then
+    printf '\e[31mPlease set APP_NAME first.\n\e[0m' && exit 1
+fi
+
+flyctl info --app "${APP_NAME}" >/tmp/${APP_NAME} 2>&1;
+if [ "$(cat /tmp/${APP_NAME} | grep -o "Could not resolve App")" = "Could not resolve App" ]; then
+    printf '\e[33mCould not resolve app. Next, create the App.\n\e[0m'
+    flyctl apps create "${APP_NAME}" >/dev/null 2>&1;
+
+    flyctl info --app "${APP_NAME}" >/tmp/${APP_NAME} 2>&1;
+    if [ "$(cat /tmp/${APP_NAME} | grep -o "Could not resolve App")" != "Could not resolve App" ]; then
+        printf '\e[32mCreate app success.\n\e[0m'
+    else
+        printf '\e[31mCreate app failed.\n\e[0m' && exit 1
+    fi
+else
+    printf '\e[33mThe app has been created.\n\e[0m'
+fi
+
+printf '\e[33mNext, create app config file - fly.toml.\n\e[0m'
+cat <<EOF >./fly.toml
+app = "$APP_NAME"
+kill_signal = "SIGINT"
+kill_timeout = 5
+processes = []
+[env]
+  Api_hash=${API_HASH}
+  Api_id=${API_ID}
+  Aria2_secret=$${ARIA2_SECRET}
+  conf=${CONF}
+  Error_user_info=${ERROR_USER}
+  PORT=${PORT}
+  Rclone_share=${RCLONE_SHARE}
+  Remote=${REMOTE}
+  Telegram_bot_api=${TE_API}
+  Telegram_user_id=${TE_ID}
+  Upload=${UPLOAD}
+[experimental]
+  allowed_public_ports = []
+  auto_rollback = true
+[[services]]
+  http_checks = []
+  internal_port = 8868
+  # processes = ["app"]
+  protocol = "tcp"
+  script_checks = []
+  [services.concurrency]
+    hard_limit = 50
+    soft_limit = 35
+    type = "connections"
+  [[services.ports]]
+    handlers = ["http"]
+    port = 80
+  [[services.ports]]
+    handlers = ["tls", "http"]
+    port = 443
+  [[services.tcp_checks]]
+    grace_period = "120s"
+    interval = "15s"
+    restart_limit = 0
+    timeout = "2s"
+EOF
+printf '\e[32mCreate app config file success.\n\e[0m'
+printf '\e[33mNext, set app secrets and regions.\n\e[0m'
+
+flyctl secrets set UUID="${UUID}"
+flyctl regions set ${REGION}
+printf '\e[32mApp secrets and regions set success. Next, deploy the app.\n\e[0m'
+flyctl deploy --detach
